4b9c38b996f447fc92a23ae2d84271fe10316788
DESKTOP-USK31OR\NGUYEN QUOC DUNG <dungnq@kstns.biz>
2026-03-24 11:51:32 +0700

    Add Project to Git

---
+++ Test_CICD/TestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using TESTCICD;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        private Program _program;
+        [SetUp]
+        public void Setup()
+        {
+            _program = new Program();
+        }
+
+        [Test]
+        public void Add_SumOf2Number()
+        {
+            var result = _program.Add(2, 3);
+            Assert.Pass();
+        }
+    }
+}
---
+++ Test_CICD/Test_CICD/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TESTCICD
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
